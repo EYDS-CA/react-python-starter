2d05c91c7c5a59964568d22717927e518842c388
Nathan Calvank <nathan@freshworks.io>
2019-10-03 10:14:51 -0700

    Configure Jest Snapshot testing with examples

--- client/src/components/CheckboxWithLabel.js
+++ client/src/components/CheckboxWithLabel.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
+// Example stateful component
 export default class CheckboxWithLabel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isChecked: false};
+    this.state = { isChecked: false };
   }
 
   onChange = () => {
-    this.setState({isChecked: !this.state.isChecked});
+    this.setState({ isChecked: !this.state.isChecked });
   };
 
   render() {
