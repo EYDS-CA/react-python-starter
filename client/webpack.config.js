23cb9e95193e4ee66fcb529ec8dcd656378532da
Nathan Calvank <nathan@freshworks.io>
2019-10-24 10:27:11 -0700

    Add SASS support

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,6 +18,17 @@ module.exports = [
           exclude: /node_modules/,
           use: ["babel-loader"],
         },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
       ],
     },
     resolve: {
@@ -58,6 +69,17 @@ module.exports = [
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: ["babel-loader"],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
         },
       ],
     },
