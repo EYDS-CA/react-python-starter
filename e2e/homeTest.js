b898f22b985864b450fe38b851975e36de60076d
Nathan Calvank <nwcalvank@gmail.com>
2019-11-25 10:27:47 -0800

    Simplify the basic FE response test

--- e2e/homeTest.js
+++ e2e/homeTest.js
@@ -1,24 +1,15 @@
 const {success, fail} = require('./helpers');
 
-// Tests that the / route loads successfully
+// Tests that the / route responds successfully
 module.exports = async browser => {
   const errors = [];
   const page = await browser.newPage();
-  await page.goto('http://localhost:3000/');
-  const header = await page.$('h1');
 
   try {
-    const headerText = await page.evaluate(
-      element => element.textContent,
-      header,
-    );
-    if (headerText.length > 0) {
-      success();
-    } else {
-      fail();
-      errors.push('Header not found');
-    }
+    await page.goto('http://localhost:3000/');
+    success();
   } catch (e) {
+    fail();
     errors.push(e);
   }
 
